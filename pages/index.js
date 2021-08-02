df60df698632e7f6de887556b61a44382e0a0b94
AtulRaj151 <atulsid151@gmail.com>
2021-08-02 17:16:36 +0100

    add modal on create blank document

--- pages/index.js
+++ pages/index.js
@@ -3,21 +3,66 @@ import Header from '../components/Header';
 import Button from "@material-tailwind/react/Button";
 import Icon from '@material-tailwind/react/Icon';
 import Image from 'next/image';
+import Modal from '@material-tailwind/react/Modal'
+import ModalBody from '@material-tailwind/react/ModalBody';
+import ModalFooter from '@material-tailwind/react/ModalFooter';
 import  {getSession, useSession} from 'next-auth/client'
 import Login from '../components/Login';
+import { useState } from 'react';
+import { size } from 'lodash';
+import { Input } from 'postcss';
 
 
 export default function Home() {
   const [session] = useSession();
+  const [ showModal ,setShowModal]  = useState(false);
+  const [input, setInput] = useState('')
 
   if(!session) return  <Login/>
+  const createDocument = ()=>{  }
+  const modal = (
+     <Modal
+       size="sm"
+       active={showModal}
+       toggler={()=>{setShowModal(false)}}
+     >
+       <ModalBody>
+         <input
+           value={input}
+           onChange={(e)=>{setInput(e.target.value)}}
+           type="text"
+           className="outline-none w-full"
+           placeholder="Enter name of document..."
+           onKeyDown={(e)=>e.key === 'Enter' && createDocument()}
+          />
+       </ModalBody>
+       <ModalFooter>
+         <Button
+          color="blue"
+          buttonType="link"
+          onClick={(e)=>{setShowModal(false)}}
+          ripple="dark"
+         >
+           Cancel
+         </Button>
+
+         <Button
+           color="blue"
+           onClick={createDocument}
+           ripple="light"
+         >Create</Button>
+       </ModalFooter>
+
+     </Modal>
+  )
   return (
     <div>
       <Head>
-        <title>Create Next App</title>
+        <title>Google Docs Clone</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
+      {modal}
       <section className="bg-[#F8F9FA] pb-10 px-10">
         <div className="max-w-3xl mx-auto">
           <div className="flex items-center justify-between py-6">
@@ -32,7 +77,7 @@ export default function Home() {
             <Icon name="more_vert" size="3xl"/>
           </div>
           <div>
-            <div className="relative h-52 w-40 border-2 cursor-pointer hover:border-blue-700">
+            <div onClick={()=>{setShowModal(true)}} className="relative h-52 w-40 border-2 cursor-pointer hover:border-blue-700">
             <Image src='https://links.papareact.com/pju' layout='fill' />
             </div>
             <p className="ml-2 mt-2 font-semibold text-sm text-gray-700">Blank</p> 
